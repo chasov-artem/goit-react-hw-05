93abb15e44052a2e95575c3fa70b564b9e77bb7f
chasov-artem <chasov90@gmail.com>
2024-09-29 20:08:29 +0300

    fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
-// import HomePage from "./Pages/HomePage/HomePage";
-// import MoviesPage from "./Pages/MoviesPage/MoviesPage";
-// import NotFound from "./Pages/NotFound/NotFound";
-// import MovieDetails from "./Pages/MovieDetails/MovieDetails";
-// import MovieCast from "./components/MovieCast/MovieCast";
-// import MovieReviews from "./components/MovieReviews/MovieReviews";
 import { lazy, Suspense } from "react";
 
 const HomePage = lazy(() => import("./Pages/HomePage/HomePage"));
