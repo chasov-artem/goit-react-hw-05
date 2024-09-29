95fa7817406c27e64ed8263bf6ead87462b5e3d3
chasov-artem <chasov90@gmail.com>
2024-09-30 01:02:26 +0300

    bug fixing!

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,8 +1,8 @@
 import { Suspense, useEffect, useRef, useState } from "react";
 import { Link, Outlet, useLocation, useParams } from "react-router-dom";
-import { fetchMovieById } from "../../../services/api";
-import Navigates from "../../../components/Navigates/Navigates";
-import Loader from "../../../components/Loader/Loader";
+import { fetchMovieById } from "../../services/api";
+import Navigates from "../../components/Navigates/Navigates";
+import Loader from "../../components/Loader/Loader";
 import s from "./MovieDetails.module.css";
 
 const MovieDetails = () => {
