b6cb48b37d84c32fa5f462511a3c748bef4d79d1
chasov-artem <chasov90@gmail.com>
2024-09-30 12:25:13 +0300

    Navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,9 @@
-import { NavLink } from "react-router-dom";
+import Navigation from "../Navigation/Navigation";
 import s from "./Header.module.css";
-import clsx from "clsx";
 const Header = () => {
-  const buildLinkClass = ({ isActive }) => {
-    return clsx(s.link, isActive && s.activeLink);
-  };
   return (
     <div className={s.headerWrapper}>
-      <div className={s.linksWrapper}>
-        <NavLink className={buildLinkClass} to="/">
-          Home
-        </NavLink>
-        <NavLink className={buildLinkClass} to="/movies">
-          Movies
-        </NavLink>
-      </div>
+      <Navigation />
     </div>
   );
 };
